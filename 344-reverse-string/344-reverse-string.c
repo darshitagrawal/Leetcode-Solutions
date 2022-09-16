6a62489c320c4392d57730d5c05ba550f77aecbd
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2022-09-15 22:29:45 -0600

    Time: 83 ms (51.93%), Space: 12.3 MB (91.77%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.c
@@ -0,0 +1,16 @@
+
+
+void reverseString(char* s, int sSize)
+{
+    int start = 0, end = sSize - 1;
+    char swap = '\0';
+    
+    while(start < end)
+    {
+        swap = s[start];
+        s[start] = s[end];
+        s[end] = swap;
+        start++;
+        end--;
+    }
+}
