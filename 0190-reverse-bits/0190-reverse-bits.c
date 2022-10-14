df74ef3e9f2056a1e5a3716ecea7dfaf98b36a14
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2022-10-14 12:57:55 -0600

    Time: 3 ms (52.59%), Space: 5.3 MB (97.20%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,16 +1,12 @@
 uint32_t reverseBits(uint32_t n) 
 {
     uint32_t power = 31, bit = 0;
-    uint32_t ret = 0;
     
-    while(n != 0)
+    while(n)
     {
-        bit = n & 1;
+        bit |= (n & 1) << power--;
         n >>= 1;
-        
-        ret |= bit << power;
-        power--;
-    }
+    }   
     
-    return ret;
+    return bit;
 }
