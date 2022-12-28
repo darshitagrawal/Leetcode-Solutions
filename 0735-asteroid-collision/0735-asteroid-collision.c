1a405b9b70963f579d3d1dfba7c5fb2d1695322d
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2022-12-28 00:56:51 -0700

    Time: 18 ms (88.46%), Space: 7 MB (7.69%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.c
+++ 0735-asteroid-collision/0735-asteroid-collision.c
@@ -1,27 +1,53 @@
-int* asteroidCollision(int* asteroids, int asteroidsSize, int* returnSize){
-    int* stack = (int*)calloc(asteroidsSize, sizeof(int));
-    int stIdx = -1, idx = 0;
-
-    /* + : right
-       - : left
-    */
-    while(idx < asteroidsSize)
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* asteroidCollision(int* asteroids, int asteroidsSize, int* returnSize)
+{
+    int top = -1, index = 0;
+    int *stack = (int*)calloc(asteroidsSize, sizeof(int));
+    
+    while(index < asteroidsSize)
     {
-        //int curr = asteroids[idx];
-        if(stIdx >=0 && stack[stIdx]>0 && asteroids[idx]<0)  /* +- will collision */
+        if(top == -1)
         {
-            if(abs(asteroids[idx]) > abs(stack[stIdx]))
-                stIdx--;
-            else if(abs(asteroids[idx]) == abs(stack[stIdx]))
-            { 
-                stIdx--; idx++; 
-            }
-            else
-                idx++;
+            top++;
+            stack[top] = asteroids[index];
         }
-        else/* same direction or -+ */
-            stack[++stIdx] = asteroids[idx++];
+       /* else if(stack[top] < 0 && asteroids[index] > 0)
+        {
+            if(abs(asteroids[index]) > abs(stack[top]))
+            {
+                stack[top] = asteroids[index];
+                top--;
+                continue;
+            }
+            else if(stack[top] + asteroids[index] == 0)
+            {
+                top--;
+            }
+        }*/
+        else if(stack[top] > 0 && asteroids[index] < 0)
+        {
+            if(abs(asteroids[index]) > abs(stack[top]))
+            {
+                stack[top] = asteroids[index];
+                top--;
+                continue;
+            }
+            else if(stack[top] + asteroids[index] == 0)
+            {
+                top--;
+            }
+        }
+        else
+        {
+            top++;
+            stack[top] = asteroids[index];
+        }
+        index++;
     }
-    *returnSize = stIdx+1;
+    
+    *returnSize = top + 1;
+    
     return stack;
 }
