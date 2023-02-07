bb44bbf8aa5ebd48b4457c4807cc6a2e848afce1
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2023-02-07 12:27:39 -0700

    Time: 3 ms (42.70%), Space: 5.7 MB (21.35%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.c
@@ -0,0 +1,19 @@
+bool hasAlternatingBits(int n)
+{
+    int curr = n & 1;
+    
+    n >>= 1;
+    
+    while(n > 0)
+    {
+        if(curr == (n & 1))
+        {
+            return false;
+        }
+        
+        curr = n & 1;
+        n >>= 1;
+    }
+    
+    return true;
+}
