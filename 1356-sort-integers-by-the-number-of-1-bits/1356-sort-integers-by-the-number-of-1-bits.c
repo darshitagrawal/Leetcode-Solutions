e9286321610f4e02ac4e5acbf13486656b7e9285
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2023-03-26 04:38:07 -0600

    Time: 46 ms (27.38%), Space: 7.8 MB (86.90%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.c
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.c
@@ -1,36 +1,49 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int countBits(int num) {
+int setBits(int num)
+{
     int count = 0;
-    while (num) {
+    
+    while(num)
+    {
         num &= (num - 1);
         count++;
     }
+    
     return count;
 }
 
-int* sortByBits(int* arr, int arrSize, int* returnSize) {
-    int i, j, temp;
-    // calculate the number of bits in each integer and store it in a separate array
-    int bits[arrSize];
-    for (i = 0; i < arrSize; i++) {
-        bits[i] = countBits(arr[i]);
+int* sortByBits(int* arr, int arrSize, int* returnSize)
+{
+    int *result = (int*)calloc(arrSize, sizeof(int));
+    int index = 0, swap = 0;
+    
+    while(index < arrSize)
+    {
+        result[index] = setBits(arr[index]);
+        index++;
     }
-    // sort the array based on the number of bits in each integer
-    for (i = 0; i < arrSize-1; i++) {
-        for (j = 0; j < arrSize-i-1; j++) {
-            if (bits[j] > bits[j+1] || (bits[j] == bits[j+1] && arr[j] > arr[j+1])) {
-                // swap the integers and their corresponding bit counts
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                temp = bits[j];
-                bits[j] = bits[j+1];
-                bits[j+1] = temp;
+    
+    for(index = 0; index < arrSize - 1; index++)
+    {
+        for(int sub_index = 0; sub_index < arrSize - index - 1; sub_index++)
+        {
+            if(result[sub_index] > result[sub_index + 1] || 
+               result[sub_index] == result[sub_index + 1] && 
+              arr[sub_index] > arr[sub_index + 1])
+            {
+                swap = arr[sub_index];
+                arr[sub_index] = arr[sub_index + 1];
+                arr[sub_index + 1] = swap;
+                swap = result[sub_index];
+                result[sub_index] = result[sub_index + 1];
+                result[sub_index + 1] = swap;
             }
         }
     }
+    
     *returnSize = arrSize;
+    
     return arr;
 }
