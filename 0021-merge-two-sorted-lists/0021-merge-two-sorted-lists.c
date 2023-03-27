d1cc04e966dd070e08a6c4a8b66f2a0763a03fd7
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2023-03-27 15:09:37 -0600

    Time: 3 ms (80.00%), Space: 6.3 MB (11.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -9,57 +9,25 @@
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
 {
-    if(list1 == NULL)
-    {
-        return list2;
-    }
+    struct ListNode *prehead = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+    struct ListNode *prevNode = prehead;
     
-    if(list2 == NULL)
+    while(list1 != NULL && list2 != NULL)
     {
-        return list1;
-    }
-    
-    struct ListNode *dummyNode = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    struct ListNode *head = dummyNode, *node = dummyNode;
-    
-    while(list1 && list2)
-    {
-        node = dummyNode;
-        
         if(list1->val <= list2->val)
         {
-            dummyNode->val = list1->val;
+            prevNode->next = list1;
             list1 = list1->next;
         }
         else
         {
-            dummyNode->val = list2->val;
+            prevNode->next = list2;
             list2 = list2->next;
         }
-        
-        dummyNode->next = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-        dummyNode = dummyNode->next;
+        prevNode = prevNode->next;
     }
     
-    while(list1)
-    {
-        node = dummyNode;
-        dummyNode->val = list1->val;
-        list1 = list1->next;
-        dummyNode->next = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-        dummyNode = dummyNode->next;
-    }
+    prevNode->next = list1 == NULL ? list2 : list1;
     
-    while(list2)
-    {
-        node = dummyNode;
-        dummyNode->val = list2->val;
-        list2 = list2->next;
-        dummyNode->next = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-        dummyNode = dummyNode->next;
-    }
-    
-    node->next = NULL;
-    
-    return head;
+    return prehead->next;
 }
