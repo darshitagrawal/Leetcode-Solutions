380d9df0fd767afe28a31d37abcce40a766f0455
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2023-03-30 23:51:56 -0600

    Time: 3 ms (99.34%), Space: 6.9 MB (57.24%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.c
+++ 0137-single-number-ii/0137-single-number-ii.c
@@ -6,11 +6,16 @@ int singleNumber(int* nums, int numsSize)
     
     while(index < numsSize)
     {
+        /*If bits appear for the first time we add it to seenOnce and don't add it to seenTwice*/
         seenOnce = ~seenTwice & (seenOnce ^ nums[index]);
+        /*If bits appear for the second time, we reomove it from the seenOnce and add it to seenTwice*/
         seenTwice = ~seenOnce & (seenTwice ^ nums[index]);
         
         index++;
+        
+        /*Note: If bits appear for the third time, we don't add it to seenOnce but remove it from seenTwice*/
     }
     
+    /*In the end seenOnce only contains bits that appeared exactly once*/
     return seenOnce;
 }
