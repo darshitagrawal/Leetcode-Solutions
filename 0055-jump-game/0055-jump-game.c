7cc2149970e76e9c8987f824542d5a976b92e30e
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2024-06-15 23:38:18 -0600

    Time: 48 ms (62.19%), Space: 8.3 MB (14.77%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.c
@@ -0,0 +1,22 @@
+
+
+bool canJump(int* nums, int numsSize)
+{    
+    int index = 0, max_reach = 0;
+    
+    while(index < numsSize)
+    {
+        //Calculating max reach for the given index
+        if(index + nums[index] > max_reach)
+        {
+            max_reach = index + nums[index];
+        }
+        if(index == max_reach)
+        {
+            break;
+        }
+        index++;
+    }
+    
+    return (max_reach >= numsSize - 1);
+}
