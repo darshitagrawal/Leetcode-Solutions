ef166092ac22a85f7a7d1920ad9348e8be11e21b
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2024-06-19 22:30:03 -0600

    Time: 0 ms (100.00%), Space: 5.6 MB (25.64%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.c
@@ -0,0 +1,11 @@
+double angleClock(int hour, int minutes) 
+{
+    double minDegree = 0, hourDegree = 0, correctTime = 0;
+    
+    minDegree = minutes * 6;
+    hourDegree = (double)(hour * 30 + (double) ((double)minutes * (double)0.5) );
+    
+    correctTime = (minDegree - hourDegree < 0) ? (hourDegree - minDegree) : (minDegree - hourDegree);
+    
+    return correctTime <= 180 ? correctTime : (360 - correctTime);
+}
