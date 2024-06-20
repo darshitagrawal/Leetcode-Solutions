772022330ca76aae4b6ff1620d7742e0766ff1ea
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2024-06-19 18:10:37 -0600

    Time: 132 ms (53.49%), Space: 25.7 MB (54.10%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.c
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.c
@@ -3,23 +3,23 @@
  */
 int* productExceptSelf(int* nums, int numsSize, int* returnSize)
 {
-    int *answer = (int*)calloc(numsSize, sizeof(int));
+    int *result = (int*)calloc(numsSize, sizeof(int));
     int index = 1, right = 1;
-    
-    answer[0] = 1;
+    result[0] = 1;
     
     while(index < numsSize)
     {
-        answer[index] = nums[index - 1] * answer[index - 1];
+        result[index] = result[index - 1] * nums[index - 1];
         index++;
     }
     
     for(index = numsSize - 1; index >= 0; index--)
     {
-        answer[index] = answer[index] * right;
+        result[index] = result[index] * right;
         right *= nums[index];
     }
     
     *returnSize = numsSize;
-    return answer;
+    
+    return result;
 }
