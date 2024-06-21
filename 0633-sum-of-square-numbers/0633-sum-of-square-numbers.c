95cdc99124958a5874f931b4d24b76498501bf8d
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2024-06-20 21:36:43 -0600

    Time: 4 ms (33.43%), Space: 5.5 MB (29.20%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.c
@@ -0,0 +1,16 @@
+bool judgeSquareSum(int c) 
+{
+    long a = 0;
+    double b = 0;
+    
+    for(a = 0; a * a <= c; a++)
+    {
+        b = sqrt(c - a * a);
+        if(b == (int)b)
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
