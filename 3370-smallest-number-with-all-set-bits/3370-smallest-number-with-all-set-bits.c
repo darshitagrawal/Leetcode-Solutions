18d39c646f676275aadb77ffd9702f35e732d6aa
darshitagrawal <89857796+darshitagrawal@users.noreply.github.com>
2024-12-07 21:11:10 -0700

    Time: 0 ms (100.00%), Space: 8.1 MB (90.25%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.c
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.c
@@ -11,3 +11,11 @@ int smallestNumber(int n)
     
     return sum;
 }
+
+
+// def strictly_greater_power_of_2_minus_1(n):
+//     # Find the smallest power of 2 greater than n
+//     power_of_2 = 1
+//     while power_of_2 <= n:
+//         power_of_2 <<= 1  # Multiply by 2 using left shift
+//     return power_of_2 - 1
